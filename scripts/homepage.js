9407069be5a9ea6ed8b21becd3e90b904b380ad5
Alexander Petranov <shasho68.5@gmail.com>
2026-03-01 13:51:52 +0200

    Add search products by keyword feature

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,7 +4,6 @@ async function fetchProducts() {
             'https://dummyjson.com/products'
         );
         const data = await response.json();
-        console.log(data.products);
         return data.products;
     } catch (error) {
         console.log('Error: ', error);
@@ -42,3 +41,48 @@ async function init() {
 
 init();
 
+const searchBox = document.querySelector('input');
+const searchButton = document.querySelector('.search-button');
+
+async function fetchProductsBySearch(keyword) {
+    try {
+        const response = await fetch(
+            `https://dummyjson.com/products/search?q=${keyword}`
+        )
+
+        const data = await response.json();
+        return data.products;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
+
+async function init2(keyword) {
+    const products = await fetchProductsBySearch(keyword);
+    
+    products.forEach(product => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product');
+
+        const productTitle = document.createElement('h1');
+        productTitle.textContent = product.title;
+
+        const productImage = document.createElement('img');
+        productImage.src = product.thumbnail;
+
+        const productPrice = document.createElement('h2');
+        productPrice.textContent = `$${product.price}`;
+
+        productDiv.appendChild(productImage);
+        productDiv.appendChild(productTitle);
+        productDiv.appendChild(productPrice);
+
+        featuredProductsDiv.appendChild(productDiv);
+    })
+}
+
+searchButton.addEventListener('click', () => {
+    console.log("Search button pressed");
+    featuredProductsDiv.innerHTML = '';
+    init2(searchBox.value);
+})
